519d3a55461fa4ceb5cc0ceca20bc9e443d552d3
Jakub Duma <jduma842@gmail.com>
2024-02-08 19:07:14 +0100

    Missing file

--- ProjectCar/ProjectCar.Data/ProjectCarDBContext.cs
+++ ProjectCar/ProjectCar.Data/ProjectCarDBContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using ProjectCar.Data.Models;
 
 namespace ProjectCar.Data
@@ -9,6 +11,7 @@ namespace ProjectCar.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Timetable> Timetables { get; set; }
         public DbSet<WW> WW { get; set; }
+        public DbSet<WZ> WZ { get; set; }
 
         // Pozostałe modele/tabele dla DB
 
@@ -19,6 +22,21 @@ namespace ProjectCar.Data
         public ProjectCarDBContext(DbContextOptions<ProjectCarDBContext> options) : base(options)
         {
             ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+
+            try
+            {
+                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if (databaseCreator != null)
+                {
+                    if (!databaseCreator.CanConnect()) databaseCreator.Create();
+                    if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
